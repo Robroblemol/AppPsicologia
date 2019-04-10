0968af8a8c43fb7718834451116f2311c27e6c01
robrobles <robertodavidrobles@gmail.com>
2019-04-10 14:33:24 +0000

    deleting files in git

--- application/logs/log-2019-04-01.php
+++
@@ -1,7 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2019-04-01 15:33:18 --> Query error: Unknown column 'family_histories.phone' in 'order clause' - Invalid query: SELECT *, name as sb068931c
-FROM `family_histories`
-JOIN `relatives` ON `family_histories`.`id_relative` = `relatives`.`id_relative`
-WHERE `id_student` = '1'
-ORDER BY `family_histories`.`phone`
