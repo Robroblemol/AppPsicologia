0fc5af6e001fc97a97258a2d192966c8ef8b6914
robrobles <robertodavidrobles@gmail.com>
2019-04-15 03:58:49 +0000

    I hate logs

--- application/logs/log-2019-04-15.php
+++
@@ -1,4 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2019-04-15 03:10:08 --> Severity: Warning --> call_user_func() expects parameter 1 to be a valid callback, class 'Main' does not have a method 'addRelatives' /home/ubuntu/workspace/application/libraries/Grocery_CRUD.php 2828
-ERROR - 2019-04-15 03:24:33 --> Severity: Warning --> call_user_func() expects parameter 1 to be a valid callback, class 'Main' does not have a method 'getRelative' /home/ubuntu/workspace/application/libraries/Grocery_CRUD.php 2828
