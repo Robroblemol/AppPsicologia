0e3a98484adae6875814e2f81545f098753de688
robrobles <robertodavidrobles@gmail.com>
2019-05-08 06:00:33 +0000

    adding cardview to home

--- application/modules/Home/views/home/header.php
+++ application/modules/Home/views/home/header.php
@@ -30,6 +30,80 @@
     <div id="content-wrapper">
 
       <div class="container-fluid">
+        
+      <div class="row">
+          <div class="col-xl-3 col-sm-6 mb-3">
+            <div class="card text-white bg-primary o-hidden h-100">
+              <div class="card-body">
+                <div class="card-body-icon">
+                  <i class="fas fa-fw fa-list"></i>
+                </div>
+                <div class="mr-5">Seguimiento psicologico</div>
+              </div>
+              <a class="card-footer text-white clearfix small z-1"
+              href="<?=base_url("index.php/Psicology_history")?>">
+                <span class="float-left">Ir a Seguimiento psicologico</span>
+                <span class="float-right">
+                  <i class="fas fa-angle-right"></i>
+                </span>
+              </a>
+            </div>
+          </div>
+          
+          <div class="col-xl-3 col-sm-6 mb-3">
+            <div class="card text-white bg-warning o-hidden h-100">
+              <div class="card-body">
+                <div class="card-body-icon">
+                  <i class="fas fa-fw fa-list"></i>
+                </div>
+                <div class="mr-5">Estudiantes</div>
+              </div>
+              <a class="card-footer text-white clearfix small z-1" 
+                href="<?=base_url("index.php/Student")?>">
+                <span class="float-left">Ir a Estudiantes</span>
+                <span class="float-right">
+                  <i class="fas fa-angle-right"></i>
+                </span>
+              </a>
+            </div>
+          </div>
+          
+          <div class="col-xl-3 col-sm-6 mb-3">
+            <div class="card text-white bg-success o-hidden h-100">
+              <div class="card-body">
+                <div class="card-body-icon">
+                  <i class="fas fa-fw fa-list"></i>
+                </div>
+                <div class="mr-5">Acudientes</div>
+              </div>
+              <a class="card-footer text-white clearfix small z-1" 
+                href="<?=base_url("index.php/Attendants")?>">
+                <span class="float-left">Ir a Acudientes</span>
+                <span class="float-right">
+                  <i class="fas fa-angle-right"></i>
+                </span>
+              </a>
+            </div>
+          </div>
+          
+          <div class="col-xl-3 col-sm-6 mb-3">
+            <div class="card text-white bg-danger o-hidden h-100">
+              <div class="card-body">
+                <div class="card-body-icon">
+                  <i class="fas fa-fw fa-list"></i>
+                </div>
+                <div class="mr-5">Citas</div>
+              </div>
+              <a class="card-footer text-white clearfix small z-1" 
+                href="<?=base_url('index.php/Appointments')?>">
+                <span class="float-left">Ir a Citas</span>
+                <span class="float-right">
+                  <i class="fas fa-angle-right"></i>
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
 
         <?php 
             //si hay sessiones flashdata se muestran!
