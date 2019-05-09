0fbe6ae196bceb136fe993ae7f87ee81a79c410a
Lina Fernanda <ferchalina88@hotmail.com>
2019-05-09 15:08:06 -0500

    Se agregaron los iconos de los botones agregar y eliminar y se cambio de posicion en Seguimiento spicologico

--- application/modules/Psicology_history/views/Form_Psicology_histories_view.php
+++ application/modules/Psicology_history/views/Form_Psicology_histories_view.php
@@ -1,21 +1,26 @@
 <div class="card mb-3">
           <div class="card-header">
-            
+
             <form action="<?=base_url("index.php/Psicology_history/setForm/add");?>" method="post">
              <i class="fas fa-table"></i>
-                Estudiantes registrados
+                Seguimiento de Estudiantes
+              </form>
+              </div>
+
+              <div class="card-body">
+                  <form action="<?=base_url("index.php/Psicology_history/setForm/add");?>" method="post">
+                     <td>
+
+
                  <button type="submit"
                         class="btn btn-primary"
                         name="add"
                         value="Agregar">
-                     <i class="fa fa-plus" aria-hidden="true"></i>
-                     Agregar
-                 </button>
-         
+                        <i class="fa fa-plus"></i>  Agregar</button>
+                </td>
             </form>
-            </div>
-            
-          <div class="card-body">
+                      <br/>
+
             <div class="table-responsive">
               <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                 <thead>
@@ -35,35 +40,41 @@
                 <tbody>
                     <?php foreach($get as $row){ ?>
                 <tr>
-                       
+
                 <td>
                     <a href="<?=base_url("index.php/Psicology_history/get_detail/$row->id_student")?>">
+                      <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fa fa-trash"></i>
+                      </a>
+                      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="alertsDropdown">
+                          <a class="dropdown-item" href="<?=base_url("index.php/Psicology_history/delete/$row->id_student/true")?>">
+                               Eliminar</a>
+                      </div>
+
+                </td>
+                <td>
                     <?=$row->n_identification;?>
-                    </a>
                 </td>
                 <td>
-                    <a href="<?=base_url("index.php/Psicology_history/get_detail/$row->id_student")?>">
-                        <?=$row->name;?>
-                    </a>
+                    <?=$row->name;?>
                 </td>
-                <td>
-                       
-                    <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      <i class="fa fa-trash"></i>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="alertsDropdown">
-                        <a class="dropdown-item" href="<?=base_url("index.php/Psicology_history/delete/$row->id_student/true")?>">
-                             Eliminar</a>
-                    </div>
-                        
-                </td>
-                
+
+              </tr>
                 <?php } ?>
                 </tbody>
               </table>
-            </div>
           </div>
+          <br/>
+          <form action="<?=base_url("index.php/Psicology_history/setForm/add");?>" method="post">
+                           <td>
+                               <button type="submit"
+                                  name="add"
+                                  class="btn btn-primary"/>
+                                  <i class="fa fa-plus"></i>  Agregar</button>
+                          </td>
+                      </form>
+        </div>
           <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
         </div>
-    
+
     </div>
