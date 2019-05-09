bccd373e8983c444a1f4cde9aff31e456e0b92ec
Lina Fernanda <ferchalina88@hotmail.com>
2019-05-09 15:39:45 -0500

    Se agregaron los iconos de los botones agregar, modificar y eliminar y se cambio de posicion en Acudientes

--- application/modules/Attendants/views/table_attendants_view.php
+++ application/modules/Attendants/views/table_attendants_view.php
@@ -1,21 +1,24 @@
 <div class="card mb-3">
           <div class="card-header">
-            
+
             <form action="<?=base_url("index.php/Attendants/setForm/add");?>" method="post">
              <i class="fas fa-table"></i>
-                Acudientes registrados
+              Registro Acudientes
+            </form>
+            </div>
+
+            <div class="card-body">
+                <form action="<?=base_url("index.php/Attendant/setForm/add");?>" method="post">
+                   <td>
                  <button type="submit"
                         class="btn btn-primary"
                         name="add"
                         value="Agregar">
-                     <i class="fa fa-plus" aria-hidden="true"></i>
-                     Agregar
-                 </button>
-         
+                     <i class="fa fa-plus"></i>  Agregar</button>
+          </td>
             </form>
-            </div>
-            
-          <div class="card-body">
+            <br/>
+
             <div class="table-responsive">
               <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                 <thead>
@@ -49,10 +52,23 @@
                 <tbody>
         <?php foreach($get as $row){ ?>
             <tr>
-                       
+
                  <td>
-                        <?=$row->id_student;?>
-                    </td>
+
+                   <a href="<?=base_url("index.php/Attendant/setForm/$row->id_student")?>">
+                      <i class="fa fa-edit"></i>Editar</a>
+                     <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                       <i class="fa fa-trash"></i>
+                     </a>
+                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="alertsDropdown">
+                         <a class="dropdown-item" href="<?=base_url("index.php/Social_economic/delete/$row->id_student/true")?>">
+                              Eliminar</a>
+                     </div>
+
+                   </td>
+                   <td>
+                       <?=$row->id_student;?>
+                   </td>
                     <td>
                         <?=$row->type;?>
                     </td>
@@ -79,20 +95,22 @@
 
                         <?=$row->email;?>
                     </td>
-                     <td>
-
-                       <a href="<?=base_url("index.php/Attendant/setForm/$row->id_relative")?>">
-                           Modificar</a>
-                        <a href="<?=base_url("index.php/Attendant/delete/$row->id_relative")?>">
-                           Eliminar</a>
-                    </td>
             </tr>
                 <?php } ?>
                 </tbody>
               </table>
             </div>
+            </br>
+            <form action="<?=base_url("index.php/Attendant/setForm/add");?>" method="post">
+                             <td>
+                                 <button type="submit"
+                                    name="add"
+                                    class="btn btn-primary"/>
+                                    <i class="fa fa-plus"></i>  Agregar</button>
+                            </td>
+                        </form>
           </div>
           <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
         </div>
-    
+
     </div>
