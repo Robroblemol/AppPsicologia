4a2969b987a3b70dfd97ccf6dc15660ef383b775
robrobles <robertodavidrobles@gmail.com>
2019-05-14 04:31:29 +0000

    adding some style to create_group view

--- application/modules/Auth/views/auth/create_group.php
+++ application/modules/Auth/views/auth/create_group.php
@@ -1,20 +1,59 @@
-<h1><?php echo lang('create_group_heading');?></h1>
-<p><?php echo lang('create_group_subheading');?></p>
 
-<div id="infoMessage"><?php echo $message;?></div>
+<!DOCTYPE html>
+<html lang="en">
 
-<?php echo form_open("Auth/create_group");?>
+<head>
 
-      <p>
-            <?php echo lang('create_group_name_label', 'group_name');?> <br />
-            <?php echo form_input($group_name);?>
-      </p>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-      <p>
-            <?php echo lang('create_group_desc_label', 'description');?> <br />
-            <?php echo form_input($description);?>
-      </p>
+  
 
-      <p><?php echo form_submit('submit', lang('create_group_submit_btn'));?></p>
+  <!-- Custom fonts for this template-->
+  <link href="<?= base_url('/template/')?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 
-<?php echo form_close();?>
+  <!-- Custom styles for this template-->
+  <link href="<?= base_url('/template/')?>css/sb-admin.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-dark">
+<div class="container">
+      <div class="card mb-3">
+      	<div class="card-header"> 
+            <h1><?php echo lang('create_group_heading');?></h1>
+
+      </div>
+      <div class="card-body">
+            <p><?php echo lang('create_group_subheading');?></p>
+            <div id="infoMessage"><?php echo $message;?></div>
+            <?php echo form_open("Auth/create_group");?>
+            
+                  <p>
+                        <?php echo lang('create_group_name_label', 'group_name');?> <br />
+                        <?php echo form_input($group_name);?>
+                  </p>
+            
+                  <p>
+                        <?php echo lang('create_group_desc_label', 'description');?> <br />
+                        <?php echo form_input($description);?>
+                  </p>
+            
+                  <p><?php echo form_submit('submit', lang('create_group_submit_btn'));?></p>
+            
+            <?php echo form_close();?>
+       </div>
+      </div>
+</div>
+<!-- Bootstrap core JavaScript-->
+  <script src="<?= base_url('/template/')?>vendor/jquery/jquery.min.js"></script>
+  <script src="<?= base_url('/template/')?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="<?= base_url('/template/')?>vendor/jquery-easing/jquery.easing.min.js"></script>
+  </body>
+
+</html>
